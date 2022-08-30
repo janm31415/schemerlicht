9c956da1d897bccf1a091b7f840bd301c14bc71e
Jan Maes <janmaes@subadmins-iMac.local>
2022-08-30 09:53:35 +0200

    runtime

---
+++ schemescript/runtime.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <string>
+
+#include "namespace.h"
+#include "schemescript_api.h"
+#include "environment.h"
+
+#include <ostream>
+#include <memory>
+
+
+COMPILER_BEGIN
+
+struct context;
+struct environment_entry;
+struct repl_data;
+
+COMPILER_SCHEMESCRIPT_API void print_last_global_variable_used(std::ostream& out, std::shared_ptr<environment<environment_entry>> env, const repl_data& rd, const context* p_ctxt);
+
+COMPILER_SCHEMESCRIPT_API void scheme_runtime(uint64_t rax, std::ostream& out, std::shared_ptr<environment<environment_entry>> env, const repl_data& rd, const context* p_ctxt);
+
+COMPILER_END
