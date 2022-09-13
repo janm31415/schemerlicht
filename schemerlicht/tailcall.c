6d2b74b9759652dd7758269f7284dc58ed6dd5ad
Jan Maes <janm31415@gmail.com>
2022-09-13 12:44:58 +0200

    tail call analysis

--- schemerlicht/tailcall.c
+++ schemerlicht/tailcall.c
@@ -12,152 +12,6 @@ typedef struct schemerlicht_analyse_tail_call_visitor
   schemerlicht_visitor* visitor;
   } schemerlicht_analyse_tail_call_visitor;
 
-/*
-static int init_tail_call_previsit_begin(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.beg.tail_position = 0;
-  return 1;
-  }
-static void init_tail_call_visit_fixnum(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.lit.lit.fx.tail_position = 0;
-  }
-static void init_tail_call_visit_flonum(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.lit.lit.fl.tail_position = 0;
-  }
-static void init_tail_call_visit_nil(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.lit.lit.nil.tail_position = 0;
-  }
-static void init_tail_call_visit_string(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.lit.lit.str.tail_position = 0;
-  }
-static void init_tail_call_visit_symbol(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.lit.lit.sym.tail_position = 0;
-  }
-static void init_tail_call_visit_true(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.lit.lit.t.tail_position = 0;
-  }
-static void init_tail_call_visit_false(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.lit.lit.f.tail_position = 0;
-  }
-static void init_tail_call_visit_nop(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.nop.tail_position = 0;
-  }
-static void init_tail_call_visit_character(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.lit.lit.ch.tail_position = 0;
-  }
-static void init_tail_call_visit_variable(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.var.tail_position = 0;
-  }
-static void init_tail_call_visit_quote(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.quote.tail_position = 0;
-  }
-static int init_tail_call_previsit_primcall(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.prim.tail_position = 0;
-  return 1;
-  }
-static int init_tail_call_previsit_funcall(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.funcall.tail_position = 0;
-  return 1;
-  }
-static int init_tail_call_previsit_foreigncall(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.foreign.tail_position = 0;
-  return 1;
-  }
-static int init_tail_call_previsit_if(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.i.tail_position = 0;
-  return 1;
-  }
-static int init_tail_call_previsit_set(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.set.tail_position = 0;
-  return 1;
-  }
-static int init_tail_call_previsit_lambda(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.lambda.tail_position = 0;
-  return 1;
-  }
-static int init_tail_call_previsit_let(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.let.tail_position = 0;
-  return 1;
-  }
-static int init_tail_call_previsit_case(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.cas.tail_position = 0;
-  return 1;
-  }
-static int init_tail_call_previsit_cond(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.cond.tail_position = 0;
-  return 1;
-  }
-static int init_tail_call_previsit_do(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_expression* e)
-  {
-  UNUSED(ctxt);
-  UNUSED(v);
-  e->expr.d.tail_position = 0;
-  return 1;
-  }
-  */
-
 static void unset_tail_call(schemerlicht_expression* e)
   {
   switch (e->type)
@@ -279,31 +133,7 @@ static schemerlicht_init_tail_call_visitor* schemerlicht_init_tail_call_visitor_
   {
   schemerlicht_init_tail_call_visitor* v = schemerlicht_new(ctxt, schemerlicht_init_tail_call_visitor);
   v->visitor = schemerlicht_visitor_new(ctxt, v);
-  v->visitor->postvisit_expression = init_tail_call_postvisit_expression;
-  /*
-  v->visitor->visit_fixnum = init_tail_call_visit_fixnum;
-  v->visitor->visit_flonum = init_tail_call_visit_flonum;
-  v->visitor->visit_nil = init_tail_call_visit_nil;
-  v->visitor->visit_string = init_tail_call_visit_string;
-  v->visitor->visit_symbol = init_tail_call_visit_symbol;
-  v->visitor->visit_true = init_tail_call_visit_true;
-  v->visitor->visit_false = init_tail_call_visit_false;
-  v->visitor->visit_nop = init_tail_call_visit_nop;
-  v->visitor->visit_character = init_tail_call_visit_character;
-  v->visitor->visit_variable = init_tail_call_visit_variable;
-  v->visitor->visit_quote = init_tail_call_visit_quote;
-  v->visitor->previsit_primcall = init_tail_call_previsit_primcall;
-  v->visitor->previsit_funcall = init_tail_call_previsit_funcall;
-  v->visitor->previsit_foreigncall = init_tail_call_previsit_foreigncall;
-  v->visitor->previsit_begin = init_tail_call_previsit_begin;
-  v->visitor->previsit_if = init_tail_call_previsit_if;
-  v->visitor->previsit_set = init_tail_call_previsit_set;
-  v->visitor->previsit_lambda = init_tail_call_previsit_lambda;
-  v->visitor->previsit_let = init_tail_call_previsit_let;
-  v->visitor->previsit_do = init_tail_call_previsit_do;
-  v->visitor->previsit_case = init_tail_call_previsit_case;
-  v->visitor->previsit_cond = init_tail_call_previsit_cond;
-  */
+  v->visitor->postvisit_expression = init_tail_call_postvisit_expression;  
   return v;
   }
 
@@ -315,20 +145,6 @@ static void schemerlicht_init_tail_call_visitor_free(schemerlicht_context* ctxt,
     schemerlicht_delete(ctxt, v);
     }
   }
-
-
-/*
-A procedure call is called a tail call if it occurs in a tail position.
-A tail position is defined recursively as follows:
-
-- the body of a procedure is in tail position
-- if a let expression is in tail position, then the body
-  of the let is in tail position
-- if the condition expression (if test conseq altern) is in tail
-  position, then the conseq and altern branches are also in tail position.
-- all other expressions are not in tail position.
-*/
-
 
 static int previsit_program(schemerlicht_context* ctxt, schemerlicht_visitor* v, schemerlicht_program* p)
   {
--- schemerlicht/tailcall.h
+++ schemerlicht/tailcall.h
@@ -4,6 +4,18 @@
 #include "schemerlicht.h"
 #include "parser.h"
 
+/*
+A procedure call is called a tail call if it occurs in a tail position.
+A tail position is defined recursively as follows:
+
+- the body of a procedure is in tail position
+- if a let expression is in tail position, then the body
+  of the let is in tail position
+- if the condition expression (if test conseq altern) is in tail
+  position, then the conseq and altern branches are also in tail position.
+- all other expressions are not in tail position.
+*/
+
 void schemerlicht_tail_call_analysis(schemerlicht_context* ctxt, schemerlicht_program* program);
 int schemerlicht_program_only_has_tail_calls(schemerlicht_context* ctxt, schemerlicht_program* program);
 
